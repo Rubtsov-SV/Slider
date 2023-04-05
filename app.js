6e82153471f01be85e53f1f5a19195f2a4dd9d35
Сергей <serega16.92@inbox.ru>
2023-04-05 10:31:52 +0500

    Перенос кода в функцию

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const slides = document.querySelectorAll('.slide');
 
-for(const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    slide.classList.add('active');
-  })
-};
+const pluginSlider = (activeSlider) => {
+  const slides = document.querySelectorAll('.slide');
 
-const clearActiveClasses = () => {
-  slides.forEach((slide) => {
-    if (slide.classList.contains('active')) {
-      slide.classList.remove('active');
-    };
-  });
+  slides[activeSlider].classList.add('active');
+
+  for(const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active');
+    })
+  };
+  
+  const clearActiveClasses = () => {
+    slides.forEach((slide) => {
+      if (slide.classList.contains('active')) {
+        slide.classList.remove('active');
+      };
+    });
+  }
 }
+
+pluginSlider(2);
